5b777c8b7dd7505f7002164f1907009b5eecd4ef
d33p0st <d33p0st@gmail.com>
2026-03-09 15:04:58 +0530

    Made

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
+ORIGIN="$PWD"
 git clone https://github.com/d33p0st/x.git
 cd x
 chmod a+x ./server.make
 ./server.make
+cd "$ORIGIN"
+rm -rf x
--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
+ORIGIN="$PWD"
 git clone https://github.com/d33p0st/x.git
 cd x
 chmod a+x ./server.update
 ./server.update
+cd "$ORIGIN"
+rm -rf x
